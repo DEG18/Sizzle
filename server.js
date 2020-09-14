78c5a3a724eb36400420bb18c3f833ed860b73c6
Luyu Zeng <zengluyu252@gmail.com>
2020-09-13 23:27:47 -0400

    server.js

--- server.js
+++ server.js
@@ -8,17 +8,12 @@ const passport = require("./authentication/passport");
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-// setup the mongodb database
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://deg-18:Mongodbalex@1z@cluster0.42abh.mongodb.net/sizzle?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-
 // middlewares for accepting post requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -26,7 +21,17 @@ if (process.env.NODE_ENV === "production") {
 // routes
 app.use(routes);
 
-// start the server
-app.listen(PORT, () => {
-  console.log(`You're being served on port ${PORT}!!!`)
-})
+// setup the mongodb database
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb+srv://deg-18:Mongodbalex@1z@cluster0.42abh.mongodb.net/sizzle?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
