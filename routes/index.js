b6ed5c26294dfa081462e6039851b2ea7ccbe221
Luyu Zeng <zengluyu252@gmail.com>
2020-09-13 23:35:57 -0400

    API Routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const passport = require("../authentication/passport");
 const apiRoutes = require("./apiRoutes");
 const authRoutes = require("../authentication/authRoutes");
 
+// API Routes
 Router.use("/api", passport.authenticate('jwt', {session: false}), apiRoutes);
 Router.use("/auth", authRoutes);
+
+// If no API routes are hit, send the React app
+router.use((req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 module.exports = Router;
