a331113bcbd8b448af890d3ee65fc5b7973349fe
Luyu Zeng <zengluyu252@gmail.com>
2020-09-14 11:23:56 -0400

    Controller for yelp api

--- controllers/yelpapi.js
+++ controllers/yelpapi.js
@@ -1,37 +1,46 @@
 const axios = require("axios");
 const db = require("../models");
+const yelp = require("yelp-fusion");
+const client = yelp.client(process.env.API_KEY);
 
 // Defining methods for the googleController
 
 // findAll searches the Google Books API and returns only the entries we haven't already saved
 
 // It also makes sure that the books returned from the API all contain a title, author, link, description, and image
-module.exports = {
-  findAll: function(req, res) {
-    const { query: params } = req;
-    axios
-      .get("https://www.googleapis.com/books/v1/volumes", {
-        params
+module.exports = function (app) {
+    app.get("/", (req, res) => {
+    // res.sendFile(path.join(__dirname, "../public/main.html"));
+    let restaurants;
+    client
+      .search({
+        term: "vegan",
+        location: "new york, ny",
       })
-      .then(results =>
-        results.data.items.filter(
-          result =>
-            result.volumeInfo.title &&
-            result.volumeInfo.infoLink &&
-            result.volumeInfo.authors &&
-            result.volumeInfo.description &&
-            result.volumeInfo.imageLinks &&
-            result.volumeInfo.imageLinks.thumbnail
-        )
-      )
-      .then(apiBooks =>
-        db.Book.find().then(dbBooks =>
-          apiBooks.filter(apiBook =>
-            dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
-          )
-        )
-      )
-      .then(books => res.json(books))
-      .catch(err => res.status(422).json(err));
-  }
+      .then((response) => {
+        restaurants = response.jsonBody.businesses.map((business) => {
+          const obj = {
+            key: business.id,
+            name: business.name,
+            url: business.url,
+            rating: business.rating,
+            address: business.location.display_address,
+            phone: business.display_phone,
+            image: business.image_url,
+            };
+            console.log(obj);
+          return obj;
+        });
+        var hbsObject = {
+          restaurant: restaurants,
+        };
+        res.render("home", hbsObject);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    // res.render("main", restaurant);
+  });
+
+
 };
